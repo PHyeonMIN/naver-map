fbe1c1d3a8edb381b5a76ff4a083dd37fe96bbaa
박현민 <dndb4902@naver.com>
2022-09-21 08:34:48 +0900

    # 네이버지도 초기설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Script
+            strategy="beforeInteractive"
+    			src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_MAP_KEY}`}
+    		></Script>
+        <Component {...pageProps} />
+      </>
+        );
 }
 
 export default MyApp
