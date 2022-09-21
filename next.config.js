5d5b4c837ec0e55e6f787aa8d11a61ae1e9f462f
박현민 <dndb4902@naver.com>
2022-09-21 17:31:16 +0900

    # cors해결

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        destination: 'https://naveropenapi.apigw.ntruss.com/:path*',
+        source: '/example/:path*',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
